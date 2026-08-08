c2df6caee4c156f87910328a9ff84f46319354fa
João Roberto <joao.filho@simplexia.com.br>
2026-08-08 17:24:35 -0300

    Adicionando um usuário inativo para sempre ser criado quando a aplicação subir

--- src/database.js
+++ src/database.js
@@ -1,13 +1,22 @@
 /** Banco em memória. Os dados são perdidos quando a aplicação reinicia. */
 module.exports = {
   // Senha inicial: 123456 (armazenada exclusivamente como hash bcrypt).
-  usuarios: [{
-    id: '00000000-0000-4000-8000-000000000001',
-    email: 'admin@admin.com',
-    senha: '$2a$10$xtqV3Pqp1ZK.lrKv3i5Piee/mk4ajBGh0MPV/NR7m9GILQErHw5gW',
-    nome: 'ADMIN',
-    ativo: true
-  }],
+  usuarios: [
+      {
+      id: '00000000-0000-4000-8000-000000000001',
+      email: 'admin@admin.com',
+      senha: '$2a$10$xtqV3Pqp1ZK.lrKv3i5Piee/mk4ajBGh0MPV/NR7m9GILQErHw5gW',
+      nome: 'ADMIN',
+      ativo: true
+      },
+      {
+      id: '00000000-0000-4000-8000-000000000002',
+      email: 'joaorobertopmf@gmail.com',
+      senha: '$2a$10$xtqV3Pqp1ZK.lrKv3i5Piee/mk4ajBGh0MPV/NR7m9GILQErHw5gW',
+      nome: 'JOÃO ROBERTO',
+      ativo: true
+      }
+  ],
   funcionarios: [],
   processamentos: [],
   historicosFuncionarios: []
